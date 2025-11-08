1f419c707118b4b4d58f9ea2b8e18cd1935c8045
Daiyaan Muhammad <daiyaan.muhammad2002@gmail.com>
2025-11-08 22:03:32 +0600

    Diff text tokenizer using tree-sitter

---
+++ tokenizer/generateDiffToken.py
@@ -0,0 +1,39 @@
+from DiffVocabulary import DiffVocabulary
+import pandas as pd
+import pickle
+import random
+import json
+
+def load_data(file_path):
+    df = pd.read_parquet(file_path)
+    df['diff_text'] = df['diff_text'].fillna('')
+    return df['diff_text'].tolist()
+
+def main():
+    file_path = "./cleaned_dataset_part2.parquet"
+    diffs = load_data(file_path)
+
+
+    diff_vocab = DiffVocabulary(target_size=75000)
+    diff_vocab.build_vocabulary(diffs)
+    with open("diff_vocab.pkl", "wb") as f:
+        pickle.dump(diff_vocab, f)
+
+    # Export tokenized samples to JSON for inspection
+    sample_size = 1000
+    sampled_indices = random.sample(range(len(diffs)), min(sample_size, len(diffs)))
+
+    diff_tokenized = []
+    for idx in sampled_indices:
+        text = diffs[idx]
+        tokens = diff_vocab.tokenize(text)
+        ids = diff_vocab.numericalize(text)
+        diff_tokenized.append({"original": text, "tokens": tokens, "ids": ids})
+
+    with open("tokenized_diffs_sample.json", "w", encoding="utf-8") as f:
+        json.dump(diff_tokenized, f, ensure_ascii=False, indent=4)
+
+    print("✅ Vocabularies saved! Tokenized samples exported to JSON.")
+
+if __name__ == "__main__":
+    main()
